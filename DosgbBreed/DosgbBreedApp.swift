cfaebaf9ac949b1a5c02ed0e9337d7df56b4c28b
Marc <dyfroedd@gmail.com>
2023-07-03 14:15:12 +0100

    Initial Commit

---
+++ DosgbBreed/DosgbBreedApp.swift
@@ -0,0 +1,17 @@
+//
+//  DosgbBreedApp.swift
+//  DosgbBreed
+//
+//  Created by Marc Edwards on 03/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct DosgbBreedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
